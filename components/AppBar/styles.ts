678b22854a371aae0afee7d4ea83039a5b069dff
maddhruv <dhruvjainpenny@gmail.com>
2020-07-16 19:01:23 +0530

    feat: StatusPhant title font

--- components/AppBar/styles.ts
+++ components/AppBar/styles.ts
@@ -1,10 +1,11 @@
 import styled, { css } from "styled-components";
 
 export const Title = styled.h1`
+  font-family: "Staatliches", cursive;
   text-transform: uppercase;
   letter-spacing: 0.1rem;
   font-size: 2.2rem;
-  font-weight: 700;
+  font-weight: 500;
   cursor: pointer;
   ${({ theme: { colors } }) => css`
     color: ${colors.primary[800]};
